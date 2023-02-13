b482a387dd7d6578eff77791192dc2441b07f298
khawarcp <khawaraneescp@gmail.com>
2023-02-13 12:35:54 +0500

    CDN's and assets added to theme.liqui

---
+++ assets/index.js
@@ -0,0 +1,79 @@
+$(document).ready(function(e) {
+    $('.sliderWrapper').slick({
+        slidesToShow: 3.8,
+        centerMode: false,
+        infinite: false,
+        slidesToScroll: 3,
+        arrows: false,
+        // mobileFirst: true,
+        dots:false,
+        responsive: [
+          {
+            breakpoint: 1260,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+            },
+            
+          },
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 2.5,
+              slidesToScroll: 1,
+            },
+            
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            },
+            
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+            
+          },
+        ]
+        
+
+    }
+    );
+    $(".sustainlibilty h2").lettering();
+})
+
+
+
+
+
+// Custom Tabbing Product Detail page
+const tabs = document.querySelectorAll('.tabbingContainer li h4');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', function() {
+    const desc = this.nextElementSibling;
+    const expanded = this.getAttribute('aria-expanded') === 'true' || false;
+
+    tabs.forEach(tab => {
+      tab.setAttribute('aria-expanded', 'false');
+      tab.nextElementSibling.style.display = 'none';
+      tab.classList.remove('active');
+    });
+
+    this.setAttribute('aria-expanded', !expanded);
+    desc.style.display = !expanded ? 'block' : 'none';
+    this.classList.toggle('active', !expanded);
+  });
+});
+
+
+$('.material-area .opener').click(function(e){
+  e.preventDefault();
+  $(this).closest('.holder').find('.images').slideToggle().css('display', 'flex');
+})
